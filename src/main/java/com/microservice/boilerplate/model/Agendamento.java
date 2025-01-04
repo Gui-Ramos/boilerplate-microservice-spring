89a9be910a06619d5add5c7ffa06d2938b435295
Guilherme Santos <36565938+Gui-Ramos@users.noreply.github.com>
2025-01-04 17:55:47 -0300

    Revert "Develop to Release"

--- src/main/java/com/microservice/boilerplate/model/Agendamento.java
+++ src/main/java/com/microservice/boilerplate/model/Agendamento.java
@@ -3,6 +3,7 @@ package com.microservice.boilerplate.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
@@ -11,12 +12,14 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "agendamento", schema = "public")
+@Table(name = "agendamento", schema = "agendamento")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Agendamento extends AbstractModel {
+
+    private LocalDate date;
 
     @Column(name = "hora_inicio")
     private LocalTime horaInicio;
